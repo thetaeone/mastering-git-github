02b0ca6d194a4aa74a697636177994f18e466790
taeone <taewon1554@gmail.com>
2022-10-13 15:47:22 +0900

    Edit index.js title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '윤태원' });
+  res.render('index', { title: '김민재' });
 });
 
 module.exports = router;
